faacba51ff2376afcc127b1beb7756912a35911f
unknown <lukas8092@gmail.com>
2021-10-02 11:53:56 +0200

    updated script

--- script.js
+++ script.js
@@ -2,30 +2,46 @@
 
 
 $(document).ready(function(){
-var videoPaths = ["v1.mp4","v2.mp4","v3.mp4","v4.mp4","v5.mp4"];
+var videoPaths = ["videos/v1.mp4","videos/v2.mp4","videos/v3.mp4","videos/v4.mp4","videos/v5.mp4"];
 var videoNames = ["Video 1","Video 2","Video 3","Video 4","Wellerman",];
 
 var videoTitle = $(".video-title");
 var video = document.getElementById('video');
 $("#next").click(nextVideo);
+$("#prev").click(prevVideo);
 
 var i = 0;
 
 video.src = videoPaths[i];
-videoTitle.text(videoNames[i]);
+changeVideoTitle();
 
 $("#video").bind("ended", nextVideo);
 
    function nextVideo(){
       if(i == videoPaths.length-1){
-         i = 0;
+         
          video.pause();
       }else{
       i++;
       video.src = videoPaths[i];
-      videoTitle.text(videoNames[i]);
+      changeVideoTitle();
       video.play();
       }
+   }
+
+   function prevVideo(){
+      if(i <= 0){
+         video.pause();
+      }else{
+      i--;
+      video.src = videoPaths[i];
+      changeVideoTitle();
+      video.play();
+      }
+   }
+   
+   function changeVideoTitle(){
+      videoTitle.text(videoNames[i]+ " ["+(i+1)+"/"+videoPaths.length+"]");
    }
    
 });
