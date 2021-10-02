6c90f0288af97be7bce7193a5d1e7d5395b3d7d6
unknown <lukas8092@gmail.com>
2021-10-02 10:43:28 +0200

    basic script

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
+
+
+
 $(document).ready(function(){
-   var i = 0;
-   $("#video").bind("ended", function() {
-      var video = document.getElementById('video');
-      if(i ==0)
-   video.src = "v2.MP4";
-   if(i == 1){
-   video.src = "v3.MP4";
-   i = -1; 
+var videoPaths = ["v1.mp4","v2.mp4","v3.mp4","v4.mp4","v5.mp4"];
+var videoNames = ["Video 1","Video 2","Video 3","Video 4","Wellerman",];
+
+var videoTitle = $(".video-title");
+var video = document.getElementById('video');
+$("#next").click(nextVideo);
+
+var i = 0;
+
+video.src = videoPaths[i];
+videoTitle.text(videoNames[i]);
+
+$("#video").bind("ended", nextVideo);
+
+   function nextVideo(){
+      if(i == videoPaths.length-1){
+         i = 0;
+         video.pause();
+      }else{
+      i++;
+      video.src = videoPaths[i];
+      videoTitle.text(videoNames[i]);
+      video.play();
+      }
    }
-   video.play();
-   i++;
-   });
-    
+   
 });
-
-function video(){
-
-   var video = document.getElementById('video');
-   video.src = "v2.MP4";
-   video.play();
-}
